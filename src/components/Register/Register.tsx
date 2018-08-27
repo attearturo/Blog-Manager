f8ef04f6c022c7f792aa92feef0e4bb3c1737413
attearturo <gomezardi@hotmail.com>
2018-08-27 15:58:55 -0500

    Avance

---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+const Register = () => {
+    return (
+        <section>
+            <div>
+                <h1>Registro</h1>
+            </div>
+            <form>
+                <input type="email" placeholder="Correo"/>
+                <input type="password" placeholder="Contraseña" />
+                <button type="submit">Registrar</button>
+            </form>
+        </section>
+    )
+
+};
+
+export defacult Register;
