4b8645f470a91ef8a3a4751744f1476803d102a7
attearturo <gomezardi@hotmail.com>
2018-09-20 12:22:37 -0500

    firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,21 @@ import 'core-js/es6/set';
 import 'raf/polyfill';
 
 import * as React from 'react';
+import { firebase } from 'firebase';
 import { render } from 'react-dom';
 
 import { Root } from './containers/Root/Root';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyChQsH8-RDWrAxtHnu4MHY6GwFM7YeiVik",
+  authDomain: "ollertid.firebaseapp.com",
+  databaseURL: "https://ollertid.firebaseio.com",
+  projectId: "ollertid",
+  storageBucket: "ollertid.appspot.com",
+  messagingSenderId: "886958896631"
+};
+firebase.initializeApp(config);
 
 render(
   <Root />,
